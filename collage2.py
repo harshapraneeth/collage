6e1ee163b835a19d3d51fa0b3049360c9ae92487
Harsha Praneeth <praneethharsha10@gmail.com>
2021-04-03 22:18:23 +0200

    a faster version

--- collage2.py
+++ collage2.py
@@ -5,25 +5,29 @@ import numpy as np
 # input variables
 output_size, target_size, cell_size = 5000, 500, 50
 output_name, target_name, input_name = 'rakul.jpg', 'target.jpg', 'input_images/input ({}).jpeg'
-n_inputs = 500
+n_inputs = 570
 
 n_cells = output_size//cell_size
 target_cell_size = target_size//n_cells
 
 # loading images into numpy arrays
+print('Loading images...',end='\r')
 inputs = tuple(cv2.resize(cv2.imread(input_name.format(i)), (cell_size, cell_size)) for i in range(1, n_inputs+1))
 target = cv2.resize(cv2.imread(target_name), (target_size, target_size))
 output = np.zeros((output_size, output_size, 3), dtype=np.uint8)
+print('Loading images... Done')
 
 # define similarity measure
 dissimilarity = lambda a, b: abs(a[0]-b[0])+abs(a[1]-b[1])+abs(a[2]-b[2])
 
 # calculating measures
+print('Calculating averages...',end='\r')
 input_avgs = [ tuple(map(int, inp.mean(axis=(0,1)))) for inp in inputs ]
 target_avgs = [
     [ tuple(map(int, target[i*target_cell_size:(i+1)*target_cell_size, j*target_cell_size:(j+1)*target_cell_size].mean(axis=(0,1))))  for j in range(n_cells) ] 
     for i in range(n_cells)
 ]
+print('Calculating averages... Done')
 
 # searching for optimal cells
 dis_table = dict()
@@ -37,11 +41,9 @@ for i in range(n_cells):
                 if d==0: min_p=ii; break
                 if d<min_d: min_d, min_p = d, ii
             dis_table[target_avgs[i][j]] = min_p
-        for ii in range(cell_size):
-            for jj in range(cell_size):
-                output[i*cell_size+ii, j*cell_size+jj] = inputs[min_p][ii, jj]
+        output[i*cell_size:(i+1)*cell_size, j*cell_size:(j+1)*cell_size] = inputs[min_p]
     percent = (i+1)*100//n_cells
-    print('Progress: [%s%s] %d%%'%('-'*int(percent*0.2), ' '*(20-int(percent*0.2)), percent), end='\r')
+    print('Search for opitmal cells - Progress: [%s%s] %d%%'%('-'*int(percent*0.2), ' '*(20-int(percent*0.2)), percent), end='\r')
 
 # save output image
 cv2.imwrite(output_name, output)
